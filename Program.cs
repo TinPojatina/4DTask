228f214f1f51e5502d416c11a490d672fc17b515
Tin Pojatina <tin.p99@gmail.com>
2023-05-15 13:52:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AssetManager
+{
+    public class Asset
+    {
+        public string šifra { get; set; }
+        public string naziv { get; set; }
+        public string mjesto_troška { get; set; }
+        public DateTime datum_aktivacije { get; set; }
+        public string smještaj { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string inDirectory = @"C:\Users\tinp9\Desktop\4DevProject\Rjesenje\IN\";
+            string outDirectory = @"C:\Users\tinp9\Desktop\4DevProject\Rjesenje\OUT\";
+            string invalidAssetsFile = Path.Combine(outDirectory, "Neispravna_Imovina.csv");
+
+            var assets = new Dictionary<string, Asset>();
+
+            foreach (string file in Directory.GetFiles(inDirectory))
+            {
+                string fileExtension = Path.GetExtension(file);
+                if (fileExtension == ".csv")
+                {
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split(';');
+                            ProcessLine(parts, invalidAssetsFile, assets);
+                        }
+                    }
+                }
+                else if (fileExtension == ".xml")
+                {
+                    XDocument doc = XDocument.Load(file);
+                    foreach (XElement element in doc.Descendants("Imovina"))
+                    {
+                        string[] parts = new string[5];
+                        parts[0] = (string)element.Element("šifra");
+                        parts[1] = (string)element.Element("naziv");
+                        parts[2] = (string)element.Element("mjesto_troška");
+                        parts[3] = (string)element.Element("datum_aktivacije");
+                        parts[4] = (string)element.Element("smještaj");
+                        ProcessLine(parts, invalidAssetsFile, assets);
+                    }
+                }
+            }
+
+            string outFile = Path.Combine(outDirectory, "Imovina.csv");
+            using (StreamWriter sw = new StreamWriter(outFile))
+            {
+                sw.WriteLine("šifra;naziv;mjesto_troška;datum_aktivacije;smještaj");
+
+                foreach (Asset asset in assets.Values.OrderBy(a => a.naziv))
+                {
+                    sw.WriteLine($"{asset.šifra};{asset.naziv};{asset.mjesto_troška};{asset.datum_aktivacije:dd.MM.yyyy};{asset.smještaj}");
+                }
+            }
+        }
+
+        static void ProcessLine(string[] parts, string invalidAssetsFile, Dictionary<string, Asset> assets)
+        {
+            if (parts.Length != 5)
+            {
+                File.AppendAllText(invalidAssetsFile, string.Join(';', parts) + Environment.NewLine);
+                return;
+            }
+
+            if (!DateTime.TryParseExact(parts[3], "dd.MM.yyyy", null, DateTimeStyles.None, out DateTime date))
+            {
+                File.AppendAllText(invalidAssetsFile, string.Join(';', parts) + Environment.NewLine);
+                return;
+            }
+
+            string code = parts[0];
+
+            if (assets.ContainsKey(code))
+            {
+                File.AppendAllText(invalidAssetsFile, string.Join(';', parts) + Environment.NewLine);
+                return;
+            }
+
+            assets[code] = new Asset
+            {
+                šifra = code,
+                naziv = parts[1],
+                mjesto_troška = parts[2],
+                datum_aktivacije = date,
+                smještaj = parts[4]
+            };
+        }
+    }
+}
